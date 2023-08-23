5c0211571b12c3da9c16a4082fb3df826f0fdd34
vuquangpham <vuquangpham2909@gmail.com>
2023-08-23 11:30:16 +0700

    feat: utils.js

---
+++ utils.js
@@ -0,0 +1,15 @@
+const fs = require('fs');
+
+/**
+ * Read file in async way
+ * @param directory
+ * @return {Promise}
+ * */
+const readFileAsync = (directory) => {
+    return new Promise((resolve, reject) => {
+        fs.readFile(directory, 'ascii', (err, data) => {
+            if(err) return reject(err);
+            resolve(data);
+        });
+    });
+};
