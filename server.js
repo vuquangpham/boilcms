0e0d9fa0d7eb1485e73c95a73b3b482e786985e9
anhTuspham <panhtu0902@gmail.com>
2023-08-26 02:00:28 +0700

    feat: #4 Create Class Category, Handles router category admin

--- server.js
+++ server.js
@@ -4,17 +4,71 @@ const path = require('path')
 // Init app
 const app = express();
 
+class CategoryParent {
+    constructor() {
+        this.baseList = [];
+    }
+    add(category){
+        this.baseList.push(category);
+    }
+    getTypeArray(){
+        return this.baseList.map(category => category.type);
+        // [ 'posts', 'pages', 'media' ]
+    }
+    check(base){
+        const indexOfType = this.getTypeArray().indexOf(base)
+        if(indexOfType !== -1){
+            return this.baseList[indexOfType].type
+        }
+        return false
+    }
+}
+class Category {
+    constructor(name,url,type) {
+        this.name = name;
+        this.url = url;
+        this.type = type
+    }
+}
+const parent = new CategoryParent();
+
+parent.add(new Category('Dashboard','/','posts'))
+parent.add(new Category('Page','/','pages'))
+parent.add(new Category('Media','/','media'))
+
 // Set up view engine
 app.set('views',path.join(__dirname,'views'));
 app.set('view engine','ejs')
 
+// Set up static file
+app.use(express.static(__dirname + '/public'));
 app.get('/',(req,res) =>{
+    res.redirect('/boiler-admin')
+})
+
+app.get('/boiler-admin',(req,res) =>{
     res.render('admin',{
-        title: 'Dashboard'
+        title: 'Dashboard',
+        username: 'AnhTu'
     })
+})
+
+app.get('/boiler-admin/:base', (req,res) =>{
+    const base = req.params.base;
+    const matchedTypeUrl = parent.check(base)
+
+    if(!matchedTypeUrl || base !== matchedTypeUrl){
+        res.redirect('/')
+    }
+    if(base === matchedTypeUrl){
+        res.render(matchedTypeUrl)
+    }
 })
 
 // Run server
 app.listen(3000, () =>{
-console.log('Server is running on port 3000')
+    console.log('Server is running on port 3000')
 })
+
+
+
