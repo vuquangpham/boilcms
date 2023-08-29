bede45f234621b0ba1ce2aa6455ca01d2a7aedaa
vuquangpham <vuquangpham2909@gmail.com>
2023-08-29 12:39:06 +0700

    feat: init env

--- server.js
+++ server.js
@@ -1,29 +1,29 @@
-const express = require('express')
-const path = require('path')
+require('dotenv').config({path: '.env'});
+const {address} = require("ip");
+
+const express = require('express');
+const path = require('path');
 
 // Init app
 const app = express();
 
 // Set up view engine
-app.set('views',path.join(__dirname,'views'));
-app.set('view engine','ejs')
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
 
 // Set up static file
-app.use(express.static(__dirname + '/public'));
-app.get('/',(req,res) =>{
-    res.redirect('/boiler-admin/dashboards')
-})
-app.get('/boiler-admin/',(req,res) =>{
-    res.redirect('/boiler-admin/dashboards')
-})
+app.use(express.static(path.join(__dirname, 'public')));
+app.get('/', (req, res) => {
+    res.redirect('/boiler-admin/dashboards');
+});
+app.get('/boiler-admin/', (req, res) => {
+    res.redirect('/boiler-admin/dashboards');
+});
 
-let boilerAdmin = require('./routes/category')
-app.use('/boiler-admin',boilerAdmin)
+let boilerAdmin = require('./routes/category');
+app.use('/boiler-admin', boilerAdmin);
 
 // Run server
-app.listen(3000, () =>{
-    console.log('Server is running on port 3000')
-})
-
-
-
+app.listen(process.env.PORT, () => {
+    console.log(`Example app listening at http://localhost:${process.env.PORT} - http://${address()}:${process.env.PORT}`);
+});
