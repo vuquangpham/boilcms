5885c9fdeaeca9d5779ba175421ea2788bc1e5f1
vuquangpham <vuquangpham2909@gmail.com>
2023-09-01 00:39:41 +0700

    feat: introduce settings type

---
+++ core/database/settings/model.js
@@ -0,0 +1,4 @@
+const mongoose = require('mongoose');
+
+const Settings = new mongoose.Schema({});
+module.exports = Settings;
--- routes/admin.js
+++ routes/admin.js
@@ -37,6 +37,12 @@ CategoryController.add(new Category({
     type: 'media',
     contentType: Type.types.MEDIA
 }));
+CategoryController.add(new Category({
+    name: 'Settings',
+    url: '/settings',
+    type: 'settings',
+    contentType: Type.types.SETTINGS
+}));
 
 /**
  * Middleware for registering variables
