80af7e970d305aaa1092a103f76667143168d618
vuquangpham <vuquangpham2909@gmail.com>
2023-09-23 17:10:13 +0700

    chore: instruction for media

--- core/database/media/model.js
+++ core/database/media/model.js
@@ -1,4 +1,13 @@
 const mongoose = require('mongoose');
 
-const Media = new mongoose.Schema({});
+const Media = new mongoose.Schema({
+    // // information here
+    // id: '', // generate
+    // url: '',
+});
+
+// 3 bai post
+// link toi 3 ID cua tam hinh
+// findMedia(id)
+// url
 module.exports = Media;
