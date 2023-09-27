b10ee51d9e8f5327d973ddf08c5528566391fd86
vuquangpham <vuquangpham2909@gmail.com>
2023-09-28 00:34:04 +0700

    fix(media): cleanup

--- core/categories/media.js
+++ core/categories/media.js
@@ -1,9 +1,44 @@
 const Category = require('../classes/category/category');
 const Type = require("../classes/utils/type");
+const {getServerHostURL, getFilenameBasedOnSize} = require("../utils/helper.utils");
+const {cropImage} = require("../utils/os.utils");
+
+// dependencies
+const path = require("path");
 
 class Media extends Category{
     constructor(config){
         super(config);
+    }
+
+    /**
+     * Validate input data to get the correct data
+     * */
+    validateInputData(request, action = 'add'){
+        // crop image
+        cropImage({
+            imageSource: path.join(request.file.path),
+            imageDestination: path.join(process.cwd(), request.file.destination),
+            scale: 'small',
+            imageOutputName: request.file.metadata.fileName,
+            imageFileExtension: request.file.metadata.fileExt
+        })
+            .then()
+            .catch(error => {
+                console.log(error);
+            });
+
+        const serverHostURL = getServerHostURL(request);
+
+        return {
+            name: request.body.name ?? request.file.fileName,
+            type: request.file.mimetype,
+            url: {
+                original: serverHostURL + '/' + request.file.path.split('\\').slice(1).join('\\'),
+                small: serverHostURL + '/' + request.file.metadata.destinationDirectory + '/' + getFilenameBasedOnSize(request.file.metadata.fileName, 'small', request.file.metadata.fileExt)
+            },
+            directory: request.file.metadata.destinationDirectory
+        };
     }
 }
 
--- routes/admin/POST/add.js
+++ routes/admin/POST/add.js
@@ -1,13 +1,3 @@
-const path = require('path')
-
-// crop image
-const {cropImage} = require('../../../core/utils/os.utils')
-const {getServerHostURL, getFilenameBasedOnSize} = require("../../../core/utils/helper.utils");
-
-const Type = require('../../../core/classes/utils/type');
-const {stringToSlug} = require("../../../core/utils/helper.utils");
-const PageBuilder = require('../../../core/database/page-builder/model');
-
 /**
  * Handle add action
  * @param {Object} request
@@ -23,40 +13,5 @@ const handleAddAction = (request, response) => {
 
     return [promise, extraData];
 };
-
-
-const validatePostType = (request) => {
-
-};
-
-/**
- * Validate media item
- * */
-const validateMediaType = (request) => {
-    cropImage({
-        imageSource: path.join(request.file.path),
-        imageDestination: path.join(process.cwd(), request.file.destination),
-        scale: 'small',
-        imageOutputName: request.file.metadata.fileName,
-        imageFileExtension: request.file.metadata.fileExt
-    })
-        .then()
-        .catch(error => {
-            console.log(error)
-        })
-
-    const serverHostURL = getServerHostURL(request);
-    const mediaObj = {
-        name: request.body.name ?? request.file.fileName,
-        type: request.file.mimetype,
-        url: {
-            original :serverHostURL + '/' + request.file.path.split('\\').slice(1).join('\\'),
-            small: serverHostURL + '/' + request.file.metadata.destinationDirectory + '/' + getFilenameBasedOnSize(request.file.metadata.fileName, 'small', request.file.metadata.fileExt)
-        },
-        directory: request.file.metadata.destinationDirectory
-    }
-
-    return mediaObj
-}
 
 module.exports = handleAddAction;
