1f05d7dc7fec96c17486f8b9c9429e821d254a08
anhTuspham <panhtu0902@gmail.com>
2023-10-13 20:59:18 +0700

    feat: add new mongoose schema User

--- core/database/user/model.js
+++ core/database/user/model.js
@@ -1,4 +1,45 @@
 const mongoose = require('mongoose');
+const validator = require('validator')
 
-const User = new mongoose.Schema({});
-module.exports = User;
+const userSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true
+    },
+    role: {
+        type: String,
+        enum: ['admin', 'user'],
+        default: 'user'
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+        validate: [validator.isEmail]
+    },
+    password: {
+        type: String,
+        required: true,
+        // Hide password from getting user
+        select: false
+    },
+    confirmPassword: {
+        type: String,
+        required: true,
+        validate: {
+            validator: function (el) {
+                return el === this.password
+            },
+        }
+    },
+    setPasswordAt: {
+        type: Date,
+        default: new Date()
+    },
+    resetPasswordToken: {
+        type: String,
+        select: false
+    },
+    resetPasswordTokenExpired: Date
+})
+module.exports = mongoose.model('User', userSchema)
