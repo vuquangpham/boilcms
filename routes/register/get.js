cfd3cd9859c021736c593da0fd307ad57eb3c7a5
anhTuspham <panhtu0902@gmail.com>
2023-10-14 16:48:31 +0700

    feat: determine the needed register file through type

--- routes/register/get.js
+++ routes/register/get.js
@@ -1,6 +1,9 @@
 const handleGetMethod = (req, res, next) => {
     const type = req.query.type;
-    res.render(`register/${type}`)
+
+    res.render(`register/${type}`,{
+        type: type
+    })
 }
 
 module.exports = handleGetMethod;
