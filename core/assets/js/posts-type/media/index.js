cb5d96af040dcc783f5d4ab0f625095a5cd91260
vuquangpham <vuquangpham2909@gmail.com>
2023-10-14 14:34:21 +0700

    chore: cleanup media file

--- core/assets/js/posts-type/media/index.js
+++ core/assets/js/posts-type/media/index.js
@@ -1,8 +1,4 @@
 import MediaPost from "./MediaPost";
-import MediaPopup from "./MediaPopup";
 
 // media posts
 document.querySelectorAll('[data-media-wrapper]').forEach(wrapper => new MediaPost(wrapper));
-
-// media popup
-document.querySelectorAll('[data-pb-component-popup]').forEach(wrapper => new MediaPopup(wrapper));
