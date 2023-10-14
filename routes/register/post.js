c0fc1fd8a64982586f89c4950b6b0e4e08a47128
anhTuspham <panhtu0902@gmail.com>
2023-10-14 16:45:51 +0700

    feat: handle PostMethod for account

--- routes/register/post.js
+++ routes/register/post.js
@@ -1,5 +1,19 @@
+const Account = require('./../../core/classes/account/account')
+const {createSendToken} = require("../../core/utils/token.utils");
 const handlePostMethod = (req, res, next) => {
+    const account = new Account();
+    const data = account.validateInputData(req)
 
+    const promise = account.add(data)
+
+    promise
+        .then(result => {
+            createSendToken(result, 200, res)
+        })
+        .catch(err => {
+            console.error(err);
+            next(err)
+        })
 }
 
 module.exports = handlePostMethod;
