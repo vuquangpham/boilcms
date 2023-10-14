ddcf70bf7f581219fb31e59a65c6a78566663313
anhTuspham <panhtu0902@gmail.com>
2023-10-14 17:09:32 +0700

    feat: update validate input for account in model, compare password

--- core/database/user/model.js
+++ core/database/user/model.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose');
 const validator = require('validator')
+const bcrypt = require('bcrypt')
 
 const userSchema = new mongoose.Schema({
     name: {
@@ -42,4 +43,27 @@ const userSchema = new mongoose.Schema({
     },
     resetPasswordTokenExpired: Date
 })
+
+// validate input between two processes adding from form and saving to database
+userSchema.pre('save', async function (next) {
+    // if password don't modify, go next middleware
+    if (!this.isModified('password')) return next();
+
+    // Hash password with const is 12
+    this.password = await bcrypt.hash(this.password, 12)
+
+    // Remove confirm password to save in database
+    this.confirmPassword = undefined
+
+    this.setPasswordAt = Date.now() - 1000;
+    next()
+})
+
+/**
+ * Check password hashed in database and password from request
+ * */
+userSchema.methods.correctPassword = async function (candidatePassword, userPassword) {
+    return await bcrypt.compare(candidatePassword, userPassword)
+}
+
 module.exports = mongoose.model('User', userSchema)
