1d6808d029dc166db25efa846139a5db374a1735
anhTuspham <panhtu0902@gmail.com>
2023-10-19 14:29:13 +0700

    feat: add feature user log-out

--- routes/register/get.js
+++ routes/register/get.js
@@ -1,15 +1,21 @@
-const {ADMIN_URL} = require("../../core/utils/config.utils");
+const {ADMIN_URL, REGISTER_URL} = require("../../core/utils/config.utils");
 const handleGetMethod = (req, res, next) => {
     const type = res.locals.accountType.name;
     const token = res.locals.token
 
-    if(token){
+    // check if user logged and user want to log-out
+    if(token && type === 'log-out'){
+        res.cookie('jwt', '', {
+            maxAge: 5 * 1000,
+            httpOnly: true
+        })
+        return res.redirect(`${REGISTER_URL}`)
+    }
+    // when user logged, can not redirect to register
+    else if(token){
         return res.redirect(`${ADMIN_URL}`)
     }
 
-    // if(type === 'log-out'){
-    //     console.log(type)
-    // }
     res.render(`register/${type}`,{
         type: type
     })
