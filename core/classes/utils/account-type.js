52f93910f7056d815998ad5048574780cf3b0cb8
anhTuspham <panhtu0902@gmail.com>
2023-10-19 12:40:48 +0700

    feat: add type log-out in account

--- core/classes/utils/account-type.js
+++ core/classes/utils/account-type.js
@@ -1,16 +1,19 @@
-class AccountType{
-    constructor(){
+class AccountType {
+    constructor() {
         this.types = {
             SIGNIN: {
                 name: 'sign-in'
             },
             SIGNUP: {
                 name: 'sign-up'
+            },
+            LOGOUT: {
+                name: 'log-out'
             }
         };
     }
 
-    getValidatedAccountType(type){
+    getValidatedAccountType(type) {
         const result = Object.values(this.types).find(instance => instance.name === type);
         return result || this.types.SIGNIN;
     }
