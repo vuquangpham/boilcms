8d5cb7f7f2bc8728df0be99b251d2c6b490aabf7
vuquangpham <vuquangpham2909@gmail.com>
2023-10-27 14:29:28 +0700

    feat(config): add CUSTOM_TEMPLATES vaariable

--- core/utils/config.utils.js
+++ core/utils/config.utils.js
@@ -1,11 +1,14 @@
 const path = require('path');
 
-// directory
+// URL
 const ADMIN_URL = 'boiler-admin';
 const REGISTER_URL = 'register';
+
+// directory
 const CORE_DIRECTORY = path.join(process.cwd(), 'core');
-const PUBLIC_DIRECTORY = path.join(process.cwd(),'public')
+const PUBLIC_DIRECTORY = path.join(process.cwd(), 'public');
+const CUSTOM_TEMPLATES_DIRECTORY = path.join(process.cwd(), 'views', 'default', 'templates');
 
 module.exports = {
-    ADMIN_URL, REGISTER_URL, CORE_DIRECTORY, PUBLIC_DIRECTORY
+    ADMIN_URL, REGISTER_URL, CORE_DIRECTORY, PUBLIC_DIRECTORY, CUSTOM_TEMPLATES_DIRECTORY
 };
