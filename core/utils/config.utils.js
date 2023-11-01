57aad5a96a1a5b15dab5c93f8ea2c657c7ceaeb2
anhTuspham <panhtu0902@gmail.com>
2023-11-01 23:42:06 +0700

    feat(config): config new url

--- core/utils/config.utils.js
+++ core/utils/config.utils.js
@@ -3,6 +3,7 @@ const path = require('path');
 // URL
 const ADMIN_URL = 'boiler-admin';
 const REGISTER_URL = 'register';
+const RESET_PASSWORD_URL = 'reset-password';
 
 // directory
 const CORE_DIRECTORY = path.join(process.cwd(), 'core');
@@ -10,5 +11,5 @@ const PUBLIC_DIRECTORY = path.join(process.cwd(), 'public');
 const CUSTOM_TEMPLATES_DIRECTORY = path.join(process.cwd(), 'views', 'default', 'templates');
 
 module.exports = {
-    ADMIN_URL, REGISTER_URL, CORE_DIRECTORY, PUBLIC_DIRECTORY, CUSTOM_TEMPLATES_DIRECTORY
+    ADMIN_URL, REGISTER_URL, RESET_PASSWORD_URL, CORE_DIRECTORY, PUBLIC_DIRECTORY, CUSTOM_TEMPLATES_DIRECTORY
 };
