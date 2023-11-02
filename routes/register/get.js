8c7c84114d2ac53da0f22a55bc5b9c6cf0b31b45
vuquangpham <vuquangpham2909@gmail.com>
2023-11-03 00:37:47 +0700

    fix(register): clear the error message after showing

--- routes/register/get.js
+++ routes/register/get.js
@@ -6,20 +6,19 @@ const {ADMIN_URL, REGISTER_URL} = require("../../core/utils/config.utils");
 const {sendEmptyToken} = require("../../core/utils/token.utils");
 const {capitalizeString} = require("../../core/utils/helper.utils");
 
-const Content = require("../../core/classes/utils/content")
-const AccountType = require('../../core/classes/utils/account-type')
-
+const Content = require("../../core/classes/utils/content");
+const AccountType = require('../../core/classes/utils/account-type');
 
 const handleGetMethod = (request, response, next) => {
-
+    // vars
     const type = response.locals.accountType.name;
-    const token = response.locals.token
-    const resetUrlToken = request.query.token
+    const token = response.locals.token;
+    const resetUrlToken = request.query.token;
 
     // check if user logged and user want to log-out
-    const logOutAction = AccountType.getActionType('log-out')
-    if (token && type === logOutAction.name) {
-        sendEmptyToken(response)
+    const logOutAction = AccountType.getActionType('log-out');
+    if(token && type === logOutAction.name){
+        sendEmptyToken(response);
         return response.redirect(`/${REGISTER_URL}`);
     }
 
@@ -27,22 +26,28 @@ const handleGetMethod = (request, response, next) => {
     else if(token) return response.redirect(`/${ADMIN_URL}`);
 
     // capitalize pageTitle
-    const pageTitle = capitalizeString(type,'-')
+    const pageTitle = capitalizeString(type, '-');
+
+    // error message
+    const errMessage = request.app.get('message');
+
+    // clear the message after showing
+    if(errMessage) request.app.set('message', undefined);
 
     // render html
-    const directory = path.join(process.cwd(), 'views', 'register', 'type', `${type}` + '.ejs')
-    Content.getHTML((directory),{type: type, resetUrlToken: resetUrlToken} )
+    const directory = path.join(process.cwd(), 'views', 'register', 'type', `${type}` + '.ejs');
+    Content.getHTML((directory), {type: type, resetUrlToken: resetUrlToken})
         .then(html => {
             response.render('register/index', {
                 content: html,
                 title: pageTitle,
-                message: request.app.get('message') || ''
-            })
+                message: errMessage
+            });
         })
         .catch(err => {
-            next(err)
-        })
+            next(err);
+        });
 
-}
+};
 
 module.exports = handleGetMethod;
