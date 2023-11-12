defda313855811a67c48329f26556538720ad776
anhTuspham <panhtu0902@gmail.com>
2023-11-13 00:40:02 +0700

    feat(product): handle delete attribute

--- core/assets/js/posts-type/product/ProductPost.js
+++ core/assets/js/posts-type/product/ProductPost.js
@@ -7,7 +7,9 @@ export default class ProductPost {
         this.elements = {
             productAttributes: wrapper.querySelector('[data-product-attributes]'),
             taxonomyProductAttributes: wrapper.querySelector('[data-taxonomy]'),
-            testAttributes: wrapper.querySelector('[data-single-attribute]')
+
+            // input
+            attributesName: wrapper.querySelector('[data-attribute-name]')
         }
 
         this.attributeIndex = 1;
@@ -15,12 +17,14 @@ export default class ProductPost {
         this.wrapper.addEventListener('click', this.handleWrapperClick.bind(this))
     }
 
+    /**
+     * Add new attribute product
+     * */
     handleAddNewAttribute() {
 
         // create child
         const newAttribute = document.createElement('div');
-        newAttribute.innerHTML = this.elements.testAttributes.outerHTML
-        newAttribute.setAttribute('data-taxonomy', '')
+        newAttribute.innerHTML = this.elements.taxonomyProductAttributes.outerHTML
         newAttribute.setAttribute('data-attribute-number', this.attributeIndex)
 
         // replace input, textarea name by the increasing index
@@ -37,18 +41,40 @@ export default class ProductPost {
         this.attributeIndex++
     }
 
+    /**
+     * Handle replace attribute name label
+     * */
+    handleReplaceAttributeName(target) {
+        // TODO when attribute input name set, then data-attribute-name is set
+    }
+
+    /**
+     * Delete attribute
+     * */
+    handleDeleteAttribute(target) {
+        const taxonomyAttribute = target.closest('[data-attribute-number]')
+        taxonomyAttribute.remove()
+    }
+
+    /**
+     * Handle click product detail
+     * */
     handleWrapperClick(e) {
         let functionHandling = () => {
         }
-        let target = null
+        let target = null;
 
-        const addNewAttributeBtnEl = e.target.closest('button[data-add-new-attribute]')
+        const addNewAttributeBtnEl = e.target.closest('button[data-add-new-attribute]');
+        const deleteAttributeBtnEl = e.target.closest('button[data-delete-attribute]');
 
         if (addNewAttributeBtnEl) {
-            functionHandling = this.handleAddNewAttribute.bind(this)
-            target = addNewAttributeBtnEl
+            functionHandling = this.handleAddNewAttribute.bind(this);
+
+        } else if (deleteAttributeBtnEl) {
+            functionHandling = this.handleDeleteAttribute.bind(this);
+            target = deleteAttributeBtnEl
         }
 
-        functionHandling(target)
+        functionHandling(target);
     }
 }
